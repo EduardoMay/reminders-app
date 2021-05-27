9886d2f44f38fa1b2930888baeed6b3b7902d854
EduardoMay <eduardo_may@outlook.com>
2021-05-26 22:00:10 -0500

    feat: add view edit priority, RA-2

---
+++ src/views/priorities/EditPriority.vue
@@ -0,0 +1,42 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button default-href="/priorities/list"></ion-back-button>
+        </ion-buttons>
+        <ion-title>Editar Prioridad</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+      <span>Formulario</span>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonBackButton,
+  IonTitle,
+  IonContent
+} from "@ionic/vue";
+
+export default defineComponent({
+  name: "EditPriority",
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonBackButton,
+    IonTitle,
+    IonContent
+  }
+});
+</script>
