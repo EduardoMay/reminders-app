39af04f3bcd4ae98f0683e20c3955a95f3077d34
EduardoMay <eduardo_may@outlook.com>
2021-05-31 21:17:50 -0500

    feat: feat: Add view reminder, RA-38

---
+++ src/views/reminders/ViewReminder.vue
@@ -0,0 +1,47 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-button slot="icon-only">
+            <ion-icon :icon="arrowBack" slot="icon-only"></ion-icon>
+          </ion-button>
+        </ion-buttons>
+        <ion-title>Title</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true" class="ion-padding"></ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonButton,
+  IonIcon
+} from "@ionic/vue";
+import { arrowBack } from "ionicons/icons";
+export default defineComponent({
+  name: "ViewReminder",
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonButtons,
+    IonButton,
+    IonIcon
+  },
+  setup() {
+    return { arrowBack };
+  }
+});
+</script>
